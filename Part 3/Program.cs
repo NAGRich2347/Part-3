bb975ac0a216630fe6776587a67ad23ced7bc3dd
Noah Richardson <RichNoah983@GoTVDSB.ca>
2020-09-22 16:46:10 -0400

    Add project files.

---
+++ Part 3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Part_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
